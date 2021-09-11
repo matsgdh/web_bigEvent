1cb0573cbb5324f97dbee910613052b8970ccb20
shyRookie <84892996@qq.com>
2021-09-11 21:41:06 +0800

    完成了登录和注册页面开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,79 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
-
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
-
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
-  form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
-    }
-  })
-
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
+$(function () {
+    //点击去注册的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
     })
-  })
 
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+    //点击去登录的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+
+    // 依托layui自定义校验规则
+    // 从layui中获取form对象
+    var form = layui.form;
+    // 优化弹出框对象
+    var layer = layui.layer;
+    // 通过form.verify()函数自定义校验规则
+    form.verify({
+        // 定义密码规则，数组格式
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        // 校验密码输入是否一致，函数形式 
+        repwd: function (value) {
+            // 从密码框拿到值，与加了repwd的表单的value比较
+            var pwd = $('.reg-box [name="password"]').val();
+            if (value !== pwd) {
+                return '两次密码输入不一致！';
+            }
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
     })
-  })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (event) {
+        // 阻止表单的默认提交行为
+        event.preventDefault();
+        // 根据接口文档发起对应请求
+        $.post('/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function (res) {
+            if (res.status !== 0) {
+                // 使用layui优化效果
+                // return alert(res.message);
+                return layer.msg(res.message);
+            }
+            // console.log('注册成功了！');
+            layer.msg("恭喜您！注册成功了！！");
+            // 注册成功跳转回登陆页面
+            $('#link_login').click();
+        })
+    })
+
+    // 监听登陆表单的提交事件
+    $('#form_login').on('submit', function (event) {
+        // 阻止表单的默认提交行为
+        event.preventDefault();
+        // 根据接口文档发起对应请求
+        $.ajax({
+            url : '/api/login',
+            method : 'POST',
+            // 快速读取表单中的数据
+            data : $(this).serialize(),
+            success : function(res){
+                if(res.status !== 0){
+                    return layer.msg('登陆失败');
+                }
+                layer.msg('登陆成功了！');
+                // 登陆成功时，将涉及权限登陆的token值存储到本地
+                localStorage.setItem('token',res.token);
+                // 跳转到后台主页
+                location.href = './index.html';
+            }
+        })
+    })
 })
